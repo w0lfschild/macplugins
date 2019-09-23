7a97f271252f91e0b32644046c5dd91510903aae
w0lfschild <aguywithlonghair@yahoo.com>
2019-09-22 18:27:17 -0700

    Update
    - Updated sample plugin
    - Updated xcode template
    - Updated readme
    - Moved/Renamed stuff from mySIMBL to MacForge

--- templates/Example Plugin/template/bundle.m
+++ templates/Example Plugin/template/bundle.m
@@ -3,7 +3,7 @@
 //  template
 //
 //  Created by Wolfgang Baird on 8/15/16.
-//  Copyright © 2016 Wolfgang Baird. All rights reserved.
+//  Copyright © 2019 Wolfgang Baird. All rights reserved.
 //
 
 /*
